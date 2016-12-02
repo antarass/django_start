748fe6820869cd7596cf1ea91d2daf8444eb78aa
antarass <catalin.anechei@gmail.com>
2016-12-02 09:47:59 +0200

    Update views.py
    Add cheks

--- books/views.py
+++ books/views.py
@@ -8,8 +8,11 @@ def search(request):
         q = request.GET['q']
         if not q:
             error = True
+        elif len(q) > 20:
+            error = True
         else:
             books = Book.objects.filter(title__icontains=q)
             return render(request, 'search_results.html',
                           {'books': books, 'query': q})
-    return render(request, 'search_form.html', {'error': error})
+    return render(request, 'search_form.html',
+        {'error': error})
